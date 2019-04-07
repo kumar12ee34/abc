d60f68c59bcc86d7f8818119b896922f90bf31bc
kumar12ee34 <45850447+kumar12ee34@users.noreply.github.com>
2019-04-07 12:38:18 +0530

    Add files via upload

---
+++ RestApiWithOauth/src/main/java/org/restapi/configuration/HelloWorldConfiguration.java
@@ -0,0 +1,13 @@
+package org.restapi.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "org.restapi")
+public class HelloWorldConfiguration {
+	
+
+}
---
+++ RestApiWithOauth/src/main/java/org/restapi/service/UserService.java
@@ -0,0 +1,25 @@
+package org.restapi.service;
+
+import java.util.List;
+
+import org.restapi.model.User;
+
+public interface UserService {
+	
+	public User findById(long id);
+	
+	public User findByName(String name);
+	
+	public void saveUser(User user);
+	
+	public void updateUser(User user);
+	
+	public void deleteUserById(long id);
+
+	public List<User> findAllUsers(); 
+	
+	public void deleteAllUsers();
+	
+	public boolean isUserExist(User user);
+	
+}
